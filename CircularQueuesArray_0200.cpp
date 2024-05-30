4a929e8b35dade00838f43ca783701fb57c3a776
Refinaldii <refinaldi9786@gmail.com>
2024-05-30 10:37:34 +0700

    MEMBUAT PERSAMAAN IF LAGI

--- CircularQueuesArray_0200.cpp
+++ CircularQueuesArray_0200.cpp
@@ -18,6 +18,13 @@ public:
         cout << endl;
 
         if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
 
         }
     }
